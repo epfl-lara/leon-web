3327ef54f5a4e990fdfaec7fd7f3f938d54875ab
Etienne Kneuss <colder@php.net>
2013-04-23 17:15:50 +0200

    Make sure script is sourced, and avoid exit in case it is

--- setupweb.sh
+++ setupweb.sh
@@ -1,6 +1,11 @@
+if [ x"${BASH_SOURCE[0]}" == x"$0" ]; then
+    echo 'Usage: source ./setupweb.sh <path-to-leon>'
+    exit;
+fi
+
 if [ $# -eq 0 ] ; then
     echo 'Usage: source ./setupweb.sh <path-to-leon>'
-    exit 1
+    return;
 fi
 
 LEON_PATH=$1
@@ -10,12 +15,12 @@ echo "LEON_PATH=$LEON_PATH"
 
 if [ ! -d "$LEON_PATH" ]; then
     echo "$LEON_PATH does not exist"
-    exit 1;
+    return;
 fi
 
 if [ ! -f "$LEON_PATH/setupenv" ]; then
     echo "$LEON_PATH/setupenv does not exist"
-    exit 1;
+    return;
 fi
 
 source $LEON_PATH/setupenv
@@ -23,25 +28,27 @@ source $LEON_PATH/setupenv
 
 if [ ! -n "$SCALA_HOME" ]; then
     echo "\$SCALA_HOME not set!"
-    exit 1;
+    return;
 fi
 
 if [ ! -f "$LEON_PATH/target/scala-2.9.2/leon_2.9.2-2.0.jar" ]; then
     echo "Could not find leon package in: $LEON_PATH/target/scala-2.9.2/leon_2.9.2-2.0.jar"
-    exit 1;
+    return;
 fi
 
 if [ ! -f "$LEON_PATH/unmanaged/64/cafebabe_2.9.2-1.2.jar" ]; then
     echo "Could not find cafebabe package in: $LEON_PATH/unmanaged/64/cafebabe_2.9.2-1.2.jar"
-    exit 1;
+    return;
 fi
 
 if [ ! -f "$LEON_PATH/unmanaged/64/scalaz3.jar" ]; then
     echo "Could not find scalaz3 package in: $LEON_PATH/unmanaged/64/scalaz3.jar"
-    exit 1;
+    return;
 fi
 
 echo "Creating symlinks to external libs..."
+
+mkdir -p lib
 
 ln -sf "$LEON_PATH/target/scala-2.9.2/leon_2.9.2-2.0.jar" lib/leon.jar
 ln -sf "$LEON_PATH/unmanaged/64/scalaz3.jar"              lib/scalaz3.jar
