b22462a6f4cc83ae2538864516462a35fc56d846
Etienne Kneuss <colder@php.net>
2013-06-01 19:59:21 +0200

    Add dependencies for condition abduction

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,10 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "org.scala-lang" % "scala-compiler" % "2.9.1",
-      "com.h2database" % "h2" % "1.3.158"
+      "com.h2database" % "h2" % "1.3.158",
+      "junit" % "junit" % "4.8" % "test",
+      "com.novocode" % "junit-interface" % "0.10-M3" % "test",
+      "com.dongxiguo" %% "zero-log" % "0.1.2"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
