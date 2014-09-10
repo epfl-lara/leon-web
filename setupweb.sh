306d243f93571ba2e05a11b5822f8ca877871208
Etienne Kneuss <colder@php.net>
2014-09-10 23:38:58 +0200

    Add Viktor's Demo

--- setupweb.sh
+++ setupweb.sh
@@ -11,20 +11,10 @@ fi
 LEON_PATH=`readlink -m $1`
 LEON_VERSION="2.3"
 
-echo "Setting up environment for web..."
-echo "LEON_PATH=$LEON_PATH"
-
 if [ ! -d "$LEON_PATH" ]; then
     echo "$LEON_PATH does not exist"
     return;
 fi
-
-if [ ! -f "$LEON_PATH/setupenv" ]; then
-    echo "$LEON_PATH/setupenv does not exist. Did you run 'sbt script' in leon project?"
-    return;
-fi
-
-source $LEON_PATH/setupenv
 
 rm -rf leon
 ln -sf $LEON_PATH leon
