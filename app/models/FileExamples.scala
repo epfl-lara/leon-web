7258037181610d2ec0ec0e7fa4d4b95dab665132
Etienne Kneuss <colder@php.net>
2015-03-23 15:28:38 +0100

    Find examples in jar or in files (depending on start/run)

--- app/models/FileExamples.scala
+++ app/models/FileExamples.scala
@@ -3,24 +3,51 @@ package models
 
 import java.io.File
 import scala.io.Source
+import java.util.zip.ZipFile
+import java.util.zip.ZipInputStream
+import scala.collection.mutable.ArrayBuffer
 
 class FileExamples(subdir: String) {
+  def titleOf(fname: String): String = {
+    val name = fname.substring(0, fname.lastIndexOf('.'))
+
+    name.split("/").last.split("_").toList.tail.mkString(" ")
+  }
+
   lazy val allExamples: List[Example] = {
-    val d = this.getClass.getResource("/resources/examples/"+subdir)
+    val target = "resources/examples/"+subdir
+    val d = this.getClass.getResource("/"+target)
 
     if (d != null) {
-      val asFile = new File(d.toURI())
+      d.getProtocol() match {
+        case "jar" =>
+          val examples = new ArrayBuffer[Example]()
 
-      asFile.listFiles().sortBy(_.getPath()).toList.map{ f =>
-        // files are "XX_Name_More.scala
-        var name = f.getName
-        // remove extension
-        name = name.substring(0, name.lastIndexOf('.'))
+          val src = this.getClass.getProtectionDomain().getCodeSource();
+          if (src ne null) {
+            val zl = src.getLocation()
+            val zf = new ZipFile(zl.getPath())
+            val zis = new ZipInputStream(zl.openStream())
+            var ze = zis.getNextEntry()
+            while(ze ne null) {
+              val name = ze.getName()
+              if (name.startsWith(target) && name.size > target.size+1) {
+                val content = Source.fromInputStream(zf.getInputStream(ze)).mkString
 
-        // remove ordering number, and replace '_' by " "
-        val title = name.split("_").toList.tail.mkString(" ")
+                examples += Example(titleOf(name), content)
+              }
+              ze = zis.getNextEntry()
+            }
 
-        Example(title, Source.fromFile(f.getPath).mkString)
+            examples.toList
+          } else {
+            Nil
+          }
+        case "file" =>
+          val asFile = new File(d.toURI())
+          asFile.listFiles().sortBy(_.getPath()).toList.map { case f =>
+            Example(titleOf(f.getName), Source.fromFile(f.getPath).mkString)
+          }
       }
     } else {
       Nil
