e976b458592ad9d3b3c53a71d10601ab82d0226a
Etienne Kneuss <colder@php.net>
2015-04-01 13:17:30 +0200

    Fix hash

--- gen_version.sh
+++ gen_version.sh
@@ -10,5 +10,5 @@ date=`(cd leon; git log --date=short --format="%ad") | head -n 1`
 if [[ $isdev -eq 0 ]]; then
     echo "Leon-dev-$hash (Built $date)" > version
 else
-    echo "Leon-$v (Built $date)" > version
+    echo "Leon-$hash (Built $date)" > version
 fi
