e50d8a846b67b2eb0d3387d1ed0bcc12036a4502
Etienne Kneuss <ekneuss@gmail.com>
2015-04-23 18:18:08 +0200

    Error on no-file, redirect /doc

--- app/controllers/Doc.scala
+++ app/controllers/Doc.scala
@@ -12,12 +12,23 @@ object Doc extends Controller {
   val dir = Play.current.configuration.getString("app.path").getOrElse("./")
   val prefix = "leon/doc/_build/html/"
 
+  def redirect = {
+    Action {
+      Redirect(routes.Doc.index)
+    }
+  }
+
   def index = {
     at("index.html")
   }
 
   def at(path: String) = Action {
-     val f = new File(dir, prefix+path)
-     Ok.sendFile(f, inline = true).withHeaders(CACHE_CONTROL -> "max-age=3600")
+    try {
+       val f = new File(dir, prefix+path)
+       Ok.sendFile(f, inline = true).withHeaders(CACHE_CONTROL -> "max-age=3600")
+    } catch {
+      case _: java.io.FileNotFoundException =>
+        NotFound("File not found")
+    }
   }
 }
