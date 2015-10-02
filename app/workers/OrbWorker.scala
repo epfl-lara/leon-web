32fd4f3369927f5ec7af6bcb14a20e253f94b4f9
ravi <ravi.kandhadai@epfl.ch>
2015-10-02 18:13:02 +0200

    Updating printing of result from Orb

--- app/workers/OrbWorker.scala
+++ app/workers/OrbWorker.scala
@@ -17,64 +17,45 @@ import models.JsonWrites
 import models.InvariantPosition
 import play.api.libs.json._
 import play.api.libs.json.Json._
+import leon.invariant.structure.FunctionUtils._
+import purescala.Expressions._
+import purescala.Common._
+import purescala.ExprOps._
+import purescala.PrettyPrinter
 
 /**
  * @author Mikael
  */
 class OrbWorker(s: ActorRef, im: InterruptManager) extends WorkerActor(s, im) with JsonWrites {
   import ConsoleProtocol._
-  
+
   def receive = {
     case OnUpdateCode(cstate) =>
-      val result = (InstrumentationPhase andThen InferInvariantsPhase).run(this.ctx)(cstate.program)
+      val result = InferInvariantsPhase.run(this.ctx)(cstate.program)
       event("invariantSearch", Map("status" -> toJson("success")))
-      val report = result.conditions.flatMap(report => report.invariant match {
+      val reports = result.conditions.flatMap(report => report.invariant match {
         case Some(s) =>
           val inv = s
           val fd = report.fd
-          import leon.invariant.structure.FunctionUtils._
-          import purescala.Expressions._
-          import purescala.Common._
-          import purescala.ExprOps._
-          
-          val newpost = // TODO : Check if method from UnfoldingTemplateSolver.scala
-            if (fd.postcondition.isDefined) {
-              val Lambda(resultBinder, _) = fd.postcondition.get
-              Lambda(resultBinder, And(fd.getPostWoTemplate, inv))
-            } else {
-              //replace #res in the invariant by a new result variable
-              val resvar = FreshIdentifier("res", fd.returnType, true)
-              // FIXME: Is this correct (ResultVariable(fd.returnType) -> resvar.toVariable))
-              val ninv = replace(Map(ResultVariable(fd.returnType) -> resvar.toVariable), inv)
-              Lambda(Seq(ValDef(resvar, Some(fd.returnType))), ninv)
-            }
-          
           val funName = InstUtil.userFunctionName(fd)
-          
-          val pos = fd.postcondition.map { _.getPos }.getOrElse(null)
-          val startCol = if(pos != null) pos.col else -1
-          val startRow = if(pos != null) pos.line else -1
-          val length = if(pos != null) pos.fullString.length() else -1
-          
-          
-          val body = InstUtil.replaceInstruVars(newpost.body, fd)
-          
+
           List(Map("name" -> toJson(funName),
-              "startCol" -> toJson(startCol),
-              "startRow" -> toJson(startRow),
-              "length" -> toJson(length),
-              "oldInvariant" -> toJson(fd.template.map(_.toString()).getOrElse("")),
-              "newInvariant" -> toJson(body.toString())))
+            "startCol" -> toJson(0),
+            "startRow" -> toJson(0),
+            "length" -> toJson(0),
+            "oldInvariant" -> toJson(fd.template.map(_.toString()).getOrElse("")),
+            "newInvariant" -> toJson(PrettyPrinter(inv))))
         case _ => Nil
       })
-      
+
       event("invariant_result",
-          Map("module" -> toJson(Module.invariant),
-              "invariants" -> toJson(report)))
+        Map("module" -> toJson(Module.invariant),
+          "invariants" -> toJson(reports),
+          "code" -> toJson(PrettyPrinter(result.finalProgramWoInstrumentation))))
     case DoCancel =>
-    
+
     case OnClientEvent(cstate, event) =>
-    
+
     case _ =>
   }
 }
