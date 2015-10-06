a2f075a3ca48876409b978ff9b69f4bd93addd63
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-10-06 17:31:00 +0200

    Add methods to retrieve a User from the database.

--- app/models/User.scala
+++ app/models/User.scala
@@ -3,6 +3,7 @@ package leon.web
 package models
 
 import anorm._
+import anorm.SqlParser._
 import play.api.db._
 import play.api.Play.current
 
@@ -12,9 +13,66 @@ case class User(profile: BasicProfile)
 
 object User {
 
-  def setup(): Unit = {
-    implicit val conn = DB.getConnection()
+  implicit val conn = DB.getConnection()
 
+  def parser = {
+    implicit def toSome[A](a: A): Option[A] = Some(a)
+
+    for {
+      providerId  <- str("provider_id")
+      userId      <- str("user_id")
+      firstName   <- str("first_name")
+      lastName    <- str("last_name")
+      fullName    <- str("full_name")
+      email       <- str("email")
+      avatarUrl   <- str("avatar_url")
+      authMethod  <- str("auth_method")
+      accessToken <- str("access_token")
+    }
+    yield User(BasicProfile(
+      providerId, userId,
+      firstName, lastName, fullName,
+      email, avatarUrl,
+      AuthenticationMethod(authMethod),
+      None,
+      OAuth2Info(accessToken),
+      None
+    ))
+  }
+
+  def findByProviderAndId(providerId: String, userId: String): Option[User] = {
+    val query = SQL"""
+      SELECT FROM users
+      WHERE user_id = $userId
+        AND provider_id = $providerId
+      LIMIT 1
+      """
+
+      query.as(parser.singleOpt)
+  }
+
+  def findById(userId: String): Option[User] = {
+    val query = SQL"""
+      SELECT FROM users
+      WHERE user_id = $userId
+      LIMIT 1
+      """
+
+      query.as(parser.singleOpt)
+  }
+
+  def findByEmailAndProvider(email: String, providerId: String): Option[User] = {
+    val query = SQL"""
+      SELECT FROM users
+      WHERE email = $email
+        AND provider_id = $providerId
+      LIMIT 1
+      """
+
+      query.as(parser.singleOpt)
+  }
+
+  def setup(): Unit = {
     SQL"""
       CREATE TABLE IF NOT EXISTS users (
         provider_id VARCHAR(40),
