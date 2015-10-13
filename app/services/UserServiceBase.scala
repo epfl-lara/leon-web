2b0cfeac9bb2e24ab8155b34aa09f4ccac87f78b
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-10-13 13:55:18 +0200

    Change error messages in UserServiceBase.

--- app/services/UserServiceBase.scala
+++ app/services/UserServiceBase.scala
@@ -13,27 +13,27 @@ import scala.concurrent.Future
 abstract class UserServiceBase extends UserService[User] {
 
   override def saveToken(token: MailToken): Future[MailToken] = {
-    throw new UnsupportedOperationException("UserService.saveToken should never be called")
+    throw new UnsupportedOperationException("UserService.saveToken is not supported")
   }
 
   override def findToken(token: String): Future[Option[MailToken]] = {
-    throw new UnsupportedOperationException("UserService.findToken should never be called")
+    throw new UnsupportedOperationException("UserService.findToken is not supported")
   }
 
   override def deleteToken(uuid: String): Future[Option[MailToken]] = {
-    throw new UnsupportedOperationException("UserService.deleteToken should never be called")
+    throw new UnsupportedOperationException("UserService.deleteToken is not supported")
   }
 
   override def deleteExpiredTokens(): Unit = {
-    throw new UnsupportedOperationException("UserService.deleteExpiredTokens should never be called")
+    throw new UnsupportedOperationException("UserService.deleteExpiredTokens is not supported")
   }
 
   override def updatePasswordInfo(user: User, info: PasswordInfo): Future[Option[BasicProfile]] = {
-    throw new UnsupportedOperationException("UserService.updatePasswordInfo should never be called")
+    throw new UnsupportedOperationException("UserService.updatePasswordInfo is not supported")
   }
 
   override def passwordInfoFor(user: User): Future[Option[PasswordInfo]] = {
-    throw new UnsupportedOperationException("UserService.passwordInfoFor should never be called")
+    throw new UnsupportedOperationException("UserService.passwordInfoFor is not supported")
   }
 
 }
