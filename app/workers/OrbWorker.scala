7607a61dc4caeec50f8e22d8f7a1604bbffde5db
ravi <ravi.kandhadai@epfl.ch>
2015-10-23 18:50:49 +0200

    A working version of leon web

--- app/workers/OrbWorker.scala
+++ app/workers/OrbWorker.scala
@@ -47,7 +47,7 @@ class OrbWorker(s: ActorRef, im: InterruptManager) extends WorkerActor(s, im) wi
 
   private var invariantOverview = Map[String, FunInvariantStatus]()
   private var allCode: Option[String] = None
-  
+
   private var inferEngine: Option[InferenceEngine] = None
 
   def receive = {
@@ -55,7 +55,7 @@ class OrbWorker(s: ActorRef, im: InterruptManager) extends WorkerActor(s, im) wi
       inferEngine.foreach(_.interrupt())
       inferEngine = None
       sender ! Cancelled(this)
-    
+
     case OnUpdateCode(cstate) =>
 
       for (fd <- cstate.functions) {
@@ -69,89 +69,53 @@ class OrbWorker(s: ActorRef, im: InterruptManager) extends WorkerActor(s, im) wi
 
       val startProg = cstate.program
       //val nctx = this.ctx.copy(reporter = new TestSilentReporter)
-      val leonctx = createLeonContext(this.ctx, s"--timeout=20") //, s"--functions=${inFun.id.name}")
-      
+      val leonctx = createLeonContext(this.ctx, s"--timeout=120", s"--minbounds") //, s"--functions=${inFun.id.name}")
+
       inferEngine match {
         case Some(engine) =>
           engine.interrupt()
+          // reinitialize the interrupt manager
+          leonctx.interruptManager.recoverInterrupt()
         case None =>
       }
-     val inferContext = new InferenceContext(startProg, leonctx)
-     val engine = (new InferenceEngine(inferContext))
-     inferEngine = Some(engine)
-      
-     Future {
-         engine.runWithTimeout(Some(
-        (ic: InferenceCondition) => {
-          ic.prettyInv match {
-            case Some(inv) =>
-              //allCode = Some(PrettyPrinter(result.finalProgramWoInstrumentation))
-              val timeExecution = ic.time
-              val allCodeWhenUpdatingThisFunction =
-                ScalaPrinter(InferenceReportUtil.pluginResultInInput(inferContext, List(ic), inferContext.inferProgram))
-              // TODO : Update ONLY the function, not all the code.
-              val funName = InstUtil.userFunctionName(ic.fd)
-              val userFun = functionByName(funName, startProg).get
-              // replace '?' in the template
-              val template = userFun.template.map { k =>
-                PrettyPrinter(simplePostTransform {
-                  case Variable(id) if id.name.startsWith("q?") =>
-                    Variable(FreshIdentifier("?", id.getType))
-                  case e => e
-                }(k))
-              }.getOrElse("")
-              val invStr = PrettyPrinter(inv)
-              invariantOverview += funName ->
-                FunInvariantStatus(Some(userFun), Some(template), Some(invStr),
-                  Some(allCodeWhenUpdatingThisFunction), timeExecution, true)
-              notifyInvariantOverview(cstate)
-            case _ => Nil
-          }
-        }))
-     } onComplete {
-       case Success(result: InferenceReport) =>
-         allCode = Some(ScalaPrinter(InferenceReportUtil.pluginResultInInput(inferContext, result.conditions, inferContext.inferProgram)))
-         println("code: "+allCode.get)
-         notifyInvariantOverview(cstate)
-       case Failure(msg) =>
-         clientLog("Failed for no reason except: " + msg)
-     }
-      
-      
-    // TODO: Ravi:
-    // Each time an invariant is found, call:
-    //   invariantOverview += funDef -> FunInvariantStatus(funDef, Some(oldInvariant), Some(newInvariant), Some(timeExecution), true)
-    //   notifyInvariantOverview(cstate)
-    // You can inspire yourself from the code below:
-    // Note that if you want to update the verification status itself, you can use the following functions (inherited)
-    //
-    //   verifOverview += f -> FunVerifStatus(f, resultsWithCex)
-    //   notifyVerifOverview(cstate)
 
-    ////////////// DELETE THIS - START (when above task implemented)
-    /*result.conditions.foreach(report => report.prettyInv match {
-        case Some(inv) =>
-          val fd = report.fd
-          val funName = InstUtil.userFunctionName(fd)
-          val oldInvariant = fd.template.map(k => PrettyPrinter(simplifyArithmetic(InstUtil.replaceInstruVars(Util.multToTimes(k), fd)))).getOrElse("")
-          val newInvariant = PrettyPrinter(inv)
-          val timeExecution = report.time
-          val allCodeWhenUpdatingThisFunction = allCode // TODO : Update ONLY the function, not all the code.
-          println(funName)
+      val inferContext = new InferenceContext(startProg, leonctx)
+      val engine = (new InferenceEngine(inferContext))
+      inferEngine = Some(engine)
 
-          invariantOverview += funName ->
-            FunInvariantStatus(Some(fd), Some(oldInvariant), Some(newInvariant), allCodeWhenUpdatingThisFunction, timeExecution, true)
-        case _ => Nil
-      })
-      // Add found=true for all invariants which were not needed.
-      for((name, fis) <- invariantOverview if fis.status == InvariantStatus.undefined) {
-        invariantOverview += name -> fis.copy(invariantFound = true)
+      Future {
+        engine.runWithTimeout(Some(
+          (ic: InferenceCondition) => {
+            ic.prettyInv match {
+              case Some(inv) =>
+                val timeExecution = ic.time
+                val funName = InstUtil.userFunctionName(ic.fd)
+                val userFun = functionByName(funName, startProg).get
+                // replace '?' in the template
+                val template = userFun.template.map { k =>
+                  PrettyPrinter(simplePostTransform {
+                    case Variable(id) if id.name.startsWith("q?") =>
+                      Variable(FreshIdentifier("?", id.getType))
+                    case e => e
+                  }(k))
+                }.getOrElse("")
+                val invStr = PrettyPrinter(inv)
+                invariantOverview += funName ->
+                  FunInvariantStatus(Some(userFun), Some(template), Some(invStr), None, timeExecution, true)
+                notifyInvariantOverview(cstate)
+              case _ => Nil
+            }
+          }))
+      } onComplete {
+        case Success(result: InferenceReport) =>
+          inferEngine = None
+          // TODO : Update ONLY the function, not all the code.
+          allCode = Some(ScalaPrinter(InferenceReportUtil.pushResultsToInput(inferContext, result.conditions)))
+          notifyInvariantOverview(cstate)
+        case Failure(msg) =>
+          inferEngine = None
+          clientLog("Failed for no reason except: " + msg)
       }
-
-      notifyInvariantOverview(cstate)*/
-    ////////////// DELETE THIS - END
-
-    case DoCancel                     =>
 
     case OnClientEvent(cstate, event) =>
 
