8bf9a04712189739f7cb288db0f4206614d7e066
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-12-24 15:02:56 +0100

    Extract picklers into their own file.

--- client/src/main/scala/leon/Handlers.scala
+++ client/src/main/scala/leon/Handlers.scala
@@ -17,64 +17,6 @@ import com.scalawarrior.scalajs.ace._
 
 object HandlersTypes {
 
-  object picklers {
-
-    import upickle.Js
-    import upickle.default._
-
-    def Bool(x: Boolean): Js.Value =
-      if (x) Js.True else Js.False
-
-    implicit val hRepositoryWriter = Writer[HRepository] {
-      case r =>
-        Js.Obj(
-          "name"          -> Js.Str(r.name),
-          "fullName"      -> Js.Str(r.fullName),
-          "owner"         -> Js.Str(r.owner),
-          "defaultBranch" -> Js.Str(r.defaultBranch)
-        )
-    }
-
-    implicit val hRepositoryReader = Reader[HRepository] {
-      case Js.Obj(
-          ("name",          Js.Str(_name)),
-          ("fullName",      Js.Str(_fullName)),
-          ("owner",         Js.Str(_owner)),
-          ("defaultBranch", Js.Str(_defaultBranch))
-        ) =>
-          new HRepository {
-            val id            = 0L
-            val name          = _name
-            val branches      = new js.Array[String]
-            val cloneURL      = ""
-            val defaultBranch = _defaultBranch
-            val fork          = false
-            val fullName      = _fullName
-            val owner         = ""
-            val size          = 0L
-            val visibility    = "public"
-          }
-    }
-
-    implicit val hBranchWriter = Writer[HBranch] {
-      case b => Js.Obj(
-        "name" -> Js.Str(b.name),
-        "sha"  -> Js.Str(b.sha)
-      )
-    }
-
-    implicit val hBranchReader = Reader[HBranch] {
-      case Js.Obj(
-          ("name", Js.Str(_name)),
-          ("sha",  Js.Str(_sha))
-        ) => new HBranch {
-        val name = _name
-        val sha  = _sha
-      }
-    }
-
-  }
-
   @ScalaJSDefined
   trait HPermalink extends js.Object {
     val link: String
