8c301736e87d1ab4fb8bc2ca9c63effc800a658e
Romain Ruetschi <romain.ruetschi@gmail.com>
2016-10-24 14:23:44 +0200

    Fix conversion of _leon_user to a User instance.

--- client/src/main/scala/leon/web/client/data/UserManager.scala
+++ client/src/main/scala/leon/web/client/data/UserManager.scala
@@ -3,44 +3,60 @@ package client
 package data
 
 import scalajs.js
+import js.annotation._
 import js.Dynamic.{global => g}
+
 import leon.web.shared._
 
 object UserManager {
-  
-  def parseIdentities(ids: js.Dynamic): Set[Identity] = {
-    val dict = ids.asInstanceOf[js.Dictionary[js.Dynamic]]
-    (for((k, i) <- dict) yield {
-      parseIdentity(i)
-    }).toSet
+
+  @ScalaJSDefined
+  trait LeonIdentity extends js.Object {
+    val serviceUserId: String
+    val provider: String
+    val firstName: String
+    val lastName: String
+    val fullName: String
+    val email: String
+    val avatarUrl: String
   }
-  
-  def parseIdentity(id: js.Dynamic): Identity = {
-    val dict = id.asInstanceOf[js.Dictionary[String]]
-    
+
+  @ScalaJSDefined
+  trait LeonUser extends js.Object {
+    val id: String
+    val main: LeonIdentity
+    val identities: js.Dictionary[LeonIdentity]
+  }
+
+  def parseIdentities(ids: js.Dictionary[LeonIdentity]): Set[Identity] = {
+    (for((k, i) <- ids) yield parseIdentity(i)).toSet
+  }
+
+  def parseIdentity(id: LeonIdentity): Identity = {
     Identity(
-      serviceUserId= ServiceUserId(dict("serviceUserId")),
-      provider=  Provider(dict("provider")),
-      firstName= dict.get("firstName"),
-      lastName=  dict.get("lastName"),
-      fullName=  dict.get("fullName"),
-      email=     dict.get("email").map(Email),
-      avatarUrl= dict.get("avatarUrl")
+      serviceUserId = ServiceUserId(id.serviceUserId),
+      provider      = Provider(id.provider),
+      firstName     = Option(id.firstName),
+      lastName      = Option(id.lastName),
+      fullName      = Option(id.fullName),
+      email         = Option(id.email).map(Email),
+      avatarUrl     = Option(id.avatarUrl)
     )
   }
-  
+
   private
-  lazy val _initial: Option[User] = {
-    val userJs = g._leon_user.asInstanceOf[js.Dictionary[js.Dynamic]]
-    
-    val user = User(
-      userId = UserId(userJs("id").asInstanceOf[String]),
-      main   = userJs.get("main").map(parseIdentity),
-      identities = userJs.get("identities").map(parseIdentities).getOrElse(Set())
-    )
-    
-    Option(user)
-  }
+  lazy val _initial: Option[User] =
+    if (g._leon_user == null) None else {
+      val userJs = g._leon_user.asInstanceOf[LeonUser]
+
+      val user = User(
+        userId = UserId(userJs.id),
+        main   = Option(userJs.main).map(parseIdentity),
+        identities = Option(userJs.identities).map(parseIdentities).getOrElse(Set())
+      )
+
+      Option(user)
+    }
 
   lazy val initial: Option[User] = _initial
 }
